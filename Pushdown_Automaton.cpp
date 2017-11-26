10938788134ffc0c26f576a5b2225cba5cbd5779
Leigh <13970250+lvancleef@users.noreply.github.com>
2017-11-26 14:57:32 -0800

    Update Pushdown_Automaton.cpp
    Edited capitalization of inserts, edited some function inputs (from UML to C++),  added comment on delete_command().

--- Pushdown_Automaton.cpp
+++ Pushdown_Automaton.cpp
@@ -1,76 +1,117 @@
-#include "Input_Alphabet.h"
-#include "Stack_Alphbet.h"
-#include "Transition_Function.h"
-#include "States.h"
-#include "Final_States.h"
-#include "Instantaneous_Description.h"
-#include "Configuration_File.h"
+#include "input_alphabet.h"
+#include "stack_alphabet.h"
+#include "transition_function.h"
+#include "states.h"
+#include "final_states.h"
+#include "instantaneous_description.h"
+#include "configuration_settings.h"
+
 #include <string>
+
 using namespace std;
-///test
+
+Pushdown_Automaton::Pushdown_Automaton()
+{
+}
+
+bool Pushdown_Automaton::is_accepted(Instantaneous_Description instantaneous_description, int number_in_current_path)
+{
+    return true;
+}
+
+void Pushdown_Automaton::load(string definition_file_name)
+{
+}
+
+bool Pushdown_Automaton::PDA_main(Configuration_Settings configuration_settings)
+{
+    return true;
+}
+
+string Pushdown_Automaton::perform_transition(Instantaneous_Description instantaneous_description, int &number_of_transitions_performed)
+{
+    if(number_of_transitions_performed == config_settings.get_maximum_transitions())
+    {
+        return true; 
+    }
+
+    perform_transition(instantaneous_description, number_of_transitions_performed++);
     
-    Pushdown_Automaton::Pushdown_Automaton(){
+    return false;
+}
 
-    }
-    bool Pushdown_Automaton::is_accepted(Instantaneous_Description instantaneous_description, int number_in_current_path){
-        return true;
-    }
-    void Pushdown_Automaton::load(string definition_file_name){
+bool Pushdown_Automaton::is_accepted(Instantaneous_Description instantaneous_description, int number_in_current_path)
+{
+    return true;
+}
 
-    }
-    bool Pushdown_Automaton::PDA_main(Configuration_Settings configuration_settings){
-
-
-        return true;
-    }
-    string Pushdown_Automaton::perform_transition(Instantaneous_Description instantaneous_description, int &number_of_transition_performed){
-
-        if(number_of_transitions_config_settings.max_transitions()){
-           return true; 
-        }
-        perform_transition(instantaneous_description, number_of_transition_performed++);
-        return false;
-    }
-    bool Pushdown_Automaton::is_accepted(in instantaneous_description Instantaneous_Description, in number_in_current_path:int){
-        return true;
-    }
-    void Pushdown_Automaton::commands(){
+void Pushdown_Automaton::commands()
+{
 //command loop
-    }
-    bool Pushdown_Automaton::is_valid_input_string(invalue: String){
-        return true;
-    }
-    void Pushdown_Automaton::print_id(in Instantaneous_descriptioninstantaneous_description){
-        cout<<"ID\n";
-    }
-    void Pushdown_Automaton::initialize_string_list(){
+}
+    
+bool Pushdown_Automaton::is_valid_input_string(string value)
+{
+    return true;
+}
 
+void Pushdown_Automaton::print_id(Instantaneous_Description instantaneous_description)
+{
+    cout<<"ID\n";
+
+    return;
+}
+
+void Pushdown_Automaton::initialize_string_list()
+{
+    return;
+}
+    
+void Pushdown_Automaton::help_command()
+{
+    cout<<"help command\n";
+}
+
+void Pushdown_Automaton::show_command()
+{
+    cout<<"show command\n";
+}
+
+void Pushdown_Automaton::view_command()
+{
+    cout<<"view command\n";
+}
+    
+void Pushdown_Automaton::list_command()
+{
+    cout<<"list command\n";
+}
+
+void Pushdown_Automaton::insert_command()
+{
+    cout<<"insert command\n";
+}
+
+// note from lvancleef
+// does this really take in a value, or are we asking
+// within the command
+void Pushdown_Automaton::delete_command(int value)
+{
+    cout<<"del command\n";
+}
+
+void Pushdown_Automaton::sort_command()
+{
+    cout<<"sort command\n";
+}
+
+void Pushdown_Automaton::set_command()
+{
+    cout<<"set command\n";
     }
-    void Pushdown_Automaton::help_command(){
-        cout<<"help command\n";
-    }
-    void Pushdown_Automaton::show_command(){
-        cout<<"show command\n";
-    }
-    void Pushdown_Automaton::view_command(){
-        cout<<"view command\n";
-    }
-    void Pushdown_Automaton::list_command(){
-        cout<<"list command\n";
-    }
-    void Pushdown_Automaton::insert_command(){
-        cout<<"insert command\n";
-    }
-    void Pushdown_Automaton::delete_command(int value){
-        cout<<"del command\n";
-    }
-    void Pushdown_Automaton::sort_command(){
-        cout<<"sort command\n";
-    }
-    void Pushdown_Automaton::set_command(){
-        cout<<"set command\n";
-    }
-    void Pushdown_Automaton::close_command(){
-        cout<<"close command\n";
-    }
+    
+void Pushdown_Automaton::close_command()
+{
+    cout<<"close command\n";
+}
 
