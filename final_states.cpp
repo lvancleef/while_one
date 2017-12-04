0a2b40ff144a1c1fd21b6e6b5c1f2c32966f2645
efren-alvarez <33705391+efren-alvarez@users.noreply.github.com>
2017-12-03 19:24:12 -0800

    Update final_states.cpp

--- final_states.cpp
+++ final_states.cpp
@@ -16,63 +16,32 @@ Final_States::Final_States()
 }
 void Final_States::load(ifstream& definition, bool& valid)
 {
-    string get_states;
+    string value;
 
-    getline(definition, get_states); // Store contents of definition in get_states
-
-    /* We need to allocate a character pointer 'c' to hold contents of
-        FINAL_STATES. */
-    char* c = new char[get_states.length() + 1];
-    strcpy(c, get_states.c_str()); // Now we 'transfer' States to c
-
-    // Here is where we check if 'FINAL_STATES:' keyword exists
-    if(get_states.substr(0, 13).compare("FINAL_STATES:") != 0) // comparing length of get_states to "FINAL_STATES:"
+	do
     {
-        cout << "Error: Keyword 'FINAL_STATES:' doesn't exist!" << endl;
-        valid = false;
-        return;
-    }
+        // Read definition file
+        if ((definition >> value))
+		{
+		    // Check for invalid characters
+			for (int i = 0; i < value.size(); i++)
+			{
+				if ((value[i] == '\\') or
+					(value[i] == '[') or
+					(value[i] == ']') or
+					(value[i] == '<') or
+					(value[i] == '>') or
+					(value[i] <= '!') or
+					(value[i] >= '~'))
+				{
+					valid = false;
+					cout << "character '" << value << "' within state is invalid" << endl;
+				}
+			}
+			final_names.push_back(value);
+		}
 
-    // Separate each state from 'FINAL_STATES' by using strtok. strtok splits string into tokens strtok (string to truncate, delimeters)
-    char* separate_states = strtok(c, " "); // Created separate_states to store each state separately instead of having the string
-    separate_states = strtok(NULL, " ");
-
-    while(separate_states != NULL)
-    {
-        string s(separate_states); //creating string s to store each token
-        final_names.push_back(s);
-        separate_states = strtok(NULL, " ");
-    }
-
-    delete[] c;
-    delete[] separate_states;
-
-    /* Check for duplicates by comparing each reference of the elements in the vector  */
-    for(int i = 0; i < (int)final_names.size(); i++)
-    {
-        string name = final_names.at(i);
-        int j = 0; // We now compare if each element in the vector is printable by using isprint()
-
-        while(isprint(name[j]))
-            j++;
-
-        if(j != (int)name.length())
-        {
-            cout << "Error: The final states are not printable characters" << endl;
-            valid = false;
-            return;
-
-        }
-        for(int k = i + 1; k < (int)final_names.size(); k++)
-        {
-            if(final_names.at(i).compare(final_names.at(k)) == 0) // If an element matches with another element, then return false
-            {
-                cout << "Error: States in final states are not unique!" << endl;
-                valid = false;
-                return;
-            }
-        }
-    }
+	} while (!definition.eof());
 }
 /* Determine if the list of final states are states that exist in the states vector
     from the states class. */
